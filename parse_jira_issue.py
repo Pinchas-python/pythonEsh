4206eb7d0c6138a8bed25d4f3fa8b30a8c0bd246
assaf-infinit-wb <assaf.balzamovich@weissbeerger.com>
2021-09-05 19:03:48 +0300

    parsing filter string

--- parse_jira_issue.py
+++ parse_jira_issue.py
@@ -1,6 +1,12 @@
 import json
+import os
 
 issue_data= open('issueData.json')
 json_data = json.load(issue_data)
-
-print(json_data)
+linked_issues = json_data["fields"]["issuelinks"]
+linked_ids = []
+for issue in linked_issues:
+    linked_ids.append(f'_{issue["id"]}_')
+filter_string = f"\"{' or '.join(linked_ids)}\""
+os.environ["TEST_FILTER"] = filter_string
+print(filter_string)
