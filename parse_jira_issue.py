8d26f27954bf9cab71395fa4608f15502654f35d
assaf-infinit-wb <assaf.balzamovich@weissbeerger.com>
2021-09-22 13:38:03 +0300

    print filter string at the end

--- parse_jira_issue.py
+++ parse_jira_issue.py
@@ -7,5 +7,5 @@ linked_issues = json_data["fields"]["issuelinks"]
 linked_ids = []
 for issue in linked_issues:
     linked_ids.append(f'_{issue["id"]}_')
-filter_string = f"\"{' or '.join(linked_ids)}\""
+filter_string = f"{' or '.join(linked_ids)}"
 print(filter_string)
